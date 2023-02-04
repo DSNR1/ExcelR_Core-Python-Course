f4d79a83f2a03fb5c7cb5bcb4e067a22c4288aca
DSNR1 <122905801+DSNR1@users.noreply.github.com>
2023-02-04 23:45:06 +0530

    Delete FilePickleWrite.py

--- FilePickleWrite.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-vaccine_details = {"Karnataka":{"Covi": 5, "Covax":2, "Sput":3, "Users":[1,2,3,4]},
-"Maharastra":{"Covi": 6, "Covax":4, "Sput":3, "Users":[1,2,3]},
-"Delhi":{"Covi": 5, "Covax":2, "Sput":3, "Users":[1,2]}}
-
-fh = open("vdstates", "wb") #"wb" = Write + Binary
-pickle.dump(vaccine_details, fh)
-fh.close()
