5b2a18379e1fec39cf938227e53e9b119c81456a
DSNR1 <122905801+DSNR1@users.noreply.github.com>
2023-02-04 23:47:22 +0530

    Delete Practice 5.py

--- Practice 5.py
+++
@@ -1,9 +0,0 @@
-runscored = 1
-
-def localFunction():
-    runscored = 2
-    print("The value of runscored in localfunction(): ", runscored)
-
-
-localFunction()
-print("The value of runscored in main: ", runscored)
